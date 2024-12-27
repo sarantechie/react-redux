dca41645a96a630a9f4de11729a1da56eeb9e8e1
sarantechie <saran.tech25@gmail.com>
2024-12-27 09:49:22 +0530

    Direct reducer in store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,21 @@
 import {createStore} from 'redux';
 import rootReducers from './reducers';
 
+//Directly using reducers in Store
+// const initialState = {count : 0};
+
+// const counterReducers = (state=initialState,action)=>{
+//     switch(action.type){
+//         case 'INCREMENT':
+//             return {...state,count:state.count+1};
+//         case 'DECREMENT':
+//             return {...state,count:state.count-1};
+//         default:
+//             return state;
+//     }
+// };
+// const store = createStore(counterReducers);
+
 const store = createStore(rootReducers);
 
 export default store;
